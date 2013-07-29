deb7f46c0cb87c71f2e1b801ba967894f7375630
Arseniy <spufru@gmail.com>
2013-07-29 19:17:58 +0000

    travis update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var crypto = require('crypto');
 var app = express();
 var port = parseInt(process.argv[2]);
 
+app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
@@ -17,7 +18,19 @@ app.get('/', function(req, res){
 
 app.post('/travis-ci', function(req, res){
   if (req.headers['authorization'] == crypto.createHash('sha256').update('spuf/rpi-cp' + process.env.TRAVIS_TOKEN).digest('hex')) {
-    travis += JSON.stringify(req.body);
+    try {
+      var data = JSON.parse(req.body.payload);
+      delete data.author_email; 
+      delete data.author_name; 
+      delete data.committer_email; 
+      delete data.committer_name; 
+      delete data.config; 
+      delete data.matrix; 
+      delete data.repository;
+      travis += JSON.stringify(data) + '<br>';
+    } catch (e) {
+      console.log('Failed to parse travis notification: ' + JSON.stringify(req.body));
+    }
   }
   res.send('OK');
 });
