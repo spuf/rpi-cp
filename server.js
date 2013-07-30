58d6a08dd9da97f305b54cfa3c458c0f82b43706
spuf <556061+spuf@users.noreply.github.com>
2013-07-31 02:42:29 +0600

    remove travis

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var crypto = require('crypto');
 
 var app = express();
 var port = process.env.PORT || parseInt(process.argv[2]);
@@ -9,35 +8,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
 var count = 0;
-var travis = '';
 
 app.get('/', function (req, res) {
     count += 1;
-    res.send('raspberry pi. [' + count + ']<hr>' + travis);
-});
-
-app.post('/travis-ci', function (req, res) {
-    if (typeof(process.env.TRAVIS_TOKEN) == 'string') {
-        var code = 'spuf/rpi-cp' + process.env.TRAVIS_TOKEN;
-        var hash = crypto.createHash('sha256').update(code).digest('hex');
-        if (req.headers['authorization'] == hash) {
-            try {
-                var data = JSON.parse(req.body.payload);
-                delete data.author_email;
-                delete data.author_name;
-                delete data.committer_email;
-                delete data.committer_name;
-                delete data.config;
-                delete data.matrix;
-                delete data.repository;
-                travis += JSON.stringify(data) + '<br>';
-            }
-            catch (e) {
-                console.log('Failed to parse travis notification: ' + JSON.stringify(req.body));
-            }
-        }
-    }
-    res.send('OK');
+    res.send('raspberry pi. [' + count + ']');
 });
 
 app.listen(port);
