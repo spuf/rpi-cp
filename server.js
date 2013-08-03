332ec62c7373fc7260a81eb0384045722b9bc8f9
Arseniy <spufru@gmail.com>
2013-08-04 02:07:55 +0600

    bower

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-
+var async = require('async');
 var fs = require('fs');
+var util = require('util');
 
 var port = process.env.PORT || parseInt(process.argv[2]);
 
@@ -12,38 +13,35 @@ app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
-var count = 0;
-
 io.enable('browser client minification');
 io.enable('browser client etag');
 io.enable('browser client gzip');
 io.set('log level', 1);
-io.sockets.on('connection', function (socket) {
-	count += 1;
-
-	var sendInfo = function () {
+io.set('origins', 'home.spuf.ru localhost:8080');
+setInterval(function () {
+	if (io.sockets.clients().length > 0) {
 		getInfo(function (info) {
-			socket.volatile.emit('info', info);
+			io.sockets.volatile.emit('info', info);
 		});
-	};
-	var handle = setInterval(sendInfo, 1000);
-	sendInfo();
-
-	socket.on('disconnect', function () {
-		clearInterval(handle);
-		count -= 1;
-	});
-});
+	}
+}, 1000);
 
 function getInfo(callback) {
-	var info = {
-		online: count,
-		date: (new Date()).toString(),
-		temp: parseInt(fs.readFileSync('/sys/class/thermal/thermal_zone0/temp')) / 1000
-	};
-	if (typeof(callback) == 'function') {
-		callback(info);
-	}
+	async.parallel({
+		online: function (callback) {
+			callback(null, io.sockets.clients().length);
+		},
+		date: function (callback) {
+			callback(null, (new Date()).toString());
+		},
+		temp: function (callback) {
+			fs.readFile('/sys/class/thermal/thermal_zone0/temp', function (err, data) {
+				callback(null, err ? 'Unknown' : util.format('%d°C', parseInt(data) / 1000));
+			});
+		}
+	}, function (err, results) {
+		callback(results);
+	});
 }
 
 server.listen(port);
