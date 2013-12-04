9f43f412aeb7af1ade0d48dad1888ea480b0667f
Arseniy <spufru@gmail.com>
2013-12-04 19:54:03 +0400

    logging

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,45 +4,77 @@ $(function () {
 
     var tasks = {};
 
-	socket.on('tasks', function (info) {
+    socket.on('tasks', function (info) {
         tasks = info;
         var task_template = _.template('<p><%= name %>: <strong id="<%= index %>"></strong></p>');
         _.each(info, function (task, index) {
             task.index = index;
             $('#content').append(task_template(task));
         });
-	});
-	socket.on('info', function (info) {
+    });
+    socket.on('info', function (info) {
         _.each(info, function (value, name) {
             switch (tasks[name].type) {
-                case 'ping': {
-                    $('#'+name).animate({
+                case 'ping':
+                {
+                    $('#' + name).animate({
                         opacity: 0.3
-                    }, 200, 'linear', function() {
+                    }, 200, 'linear', function () {
                         $(this).text(value).animate({
                             opacity: 1
                         }, 300, 'linear');
                     });
                     break;
                 }
-                default: {
-                    $('#'+name).text(value);
+                default:
+                {
+                    $('#' + name).text(value);
                 }
             }
         });
-	});
+    });
 
-	socket.on('online', function (info) {
-		$('#online').text(info);
-	});
+    socket.on('online', function (info) {
+        $('#online').text(info);
+    });
 
-	socket.on('connect', function() {
-		$('#online').text('Connected');
+    // service connection
+
+    socket.on('connect', function () {
+        $('#online').text('Connected');
         $('#content').empty();
-	});
+        console.info('connect');
+    });
 
-	socket.on('disconnect', function() {
-		$('#online').text('Disconnected');
-	});
+    socket.on('error', function (error) {
+        $('#online').text('Error');
+        console.error('error', error);
+    });
+
+    socket.on('disconnect', function () {
+        $('#online').text('Disconnected');
+        console.error('disconnect');
+    });
+
+    socket.on('connect_error', function (error) {
+        $('#online').text('Connect error');
+        console.error('connect_error', error);
+    });
+    socket.on('connect_timeout', function () {
+        $('#online').text('Connect timeout');
+        console.warn('connect_timeout');
+    });
+    socket.on('reconnect', function (attempt) {
+        $('#online').text('Reconnect');
+        console.warn('reconnect', attempt);
+    });
+    socket.on('reconnect_error', function (error) {
+        $('#online').text('Reconnect error');
+        console.error('reconnect_error', error);
+    });
+    socket.on('reconnect_failed', function () {
+        $('#online').text('Reconnect failed');
+        console.error('reconnect_failed');
+    });
 
 });
